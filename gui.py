e6c76ddc67e8f9b91ab66c3c85aa5c297f0a6ac6
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2016-02-20 16:37:00 +0100

    * Implemented serial server * Connected and implemented top menu but help and exit * Connected to recording button

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 from tkinter import ttk
 from tkinter import *
+import tkinter.filedialog as tkd
+from pandas.core.ops import _TimeOp
+
 import serial_server as ss
+import multiprocessing as mp
+
 
 class CommandRunnable:
     def __init__(self):
@@ -73,6 +78,13 @@ class SerialSelectWidget(ttk.Labelframe, CommandRunnable):
         else:
             self.connect_button_text.set(self._connect_btn_label[0])
 
+    def serial_connect_button_label(self, state):
+        try:
+            label = self._connect_btn_label[state]
+        except IndexError:
+            return
+        self.connect_button_text.set(label)
+
 
 class RecordPlayDataWidget(ttk.Labelframe, CommandRunnable):
     def __init__(self, master, label: str = ""):
@@ -88,6 +100,9 @@ class RecordPlayDataWidget(ttk.Labelframe, CommandRunnable):
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=1)
+
+        self.set_button_play_enable(False)
+        self.set_button_record_enable(False)
 
     def _init_buttons(self):
         self.record_button = ttk.Button(self, textvariable=self.record_button_text, command=self.button_record)
@@ -122,17 +137,29 @@ class RecordPlayDataWidget(ttk.Labelframe, CommandRunnable):
         else:
             self.play_button["state"] = DISABLED
 
+    def button_play_label(self, state):
+        try:
+            label = self._play_btn_label[state]
+        except IndexError:
+            return
+        self.play_button_text.set(label)
+
 
 class MainWindow(ttk.Frame, CommandRunnable):
-    def __init__(self, master=None):
+    def __init__(self, master=None, sendTimeInterval=1):
         ttk.Frame.__init__(self, master)
         CommandRunnable.__init__(self)
+        self.recv_data = mp.Queue()
+        self.load_data = []
+        self.isPlaying = False
+        self.sendTimeInterval = 20#sendTimeInterval
+        self.server = ss.SerialServer(queue=self.recv_data, baudrate=115200)
         self.status_bar = StatusBar(master)
         self.serial_widget = SerialSelectWidget(master=self, label="Serial Select")
         self.record_play_widget = RecordPlayDataWidget(master=self, label="Data interaction")
         self.master = master
         self.master.option_add('*tearOff', False)
-        self.serial_widget.commands["serial_refresh"] = lambda: (l.device for l in ss.SerialServer.list_ports())
+        self.server.start()
 
         self._init_menu_bar()
         self._init_widget_body()
@@ -142,27 +169,119 @@ class MainWindow(ttk.Frame, CommandRunnable):
         top_menu = Menu(self.master)
         self.master["menu"] = top_menu
         file_menu = Menu(top_menu)
+        data_menu = Menu(top_menu)
         help_menu = Menu(top_menu)
 
         top_menu.add_cascade(menu=file_menu, label="File")
+        top_menu.add_cascade(menu=data_menu, label="Data")
         top_menu.add_cascade(menu=help_menu, label="Help")
 
-        file_menu.add_command(label="Open", command=self.menu_open)
-        file_menu.add_command(label="Save current data", command=self.menu_save)
+        file_menu.add_command(label="Open data", command=self.menu_open)
+        file_menu.add_command(label="Save data", command=self.menu_save)
         file_menu.add_separator()
         file_menu.add_command(label="Exit", command=exit)
 
+        data_menu.add_command(label="Clear recorded data", command=self.menu_clear_recv_data)
+        data_menu.add_command(label="Clear loaded data", command=self.menu_clear_load_data)
+
         help_menu.add_command(label="About", command=self.menu_about)
 
+        self.commands["menu_open"] = self._load_data
+
     def _init_widget_body(self):
+        self.serial_widget.commands["serial_refresh"] = lambda: (l.device for l in ss.SerialServer.list_ports())
+        self.serial_widget.commands["serial_connect"] = self._connect_serial
         self.serial_widget.pack(fill=X)
+        self.record_play_widget.commands["button_record"] = self._record_data
+        self.record_play_widget.commands["button_play"] = self._play_data
         self.record_play_widget.pack(fill=X)
+
+    def _connect_serial(self, port):
+        if not self.server.is_server_pending():
+            self.server.set_port(port)
+            self.server.start_recv()
+            self.record_play_widget.set_button_record_enable(True)
+            self.record_play_widget.set_button_play_enable(True)
+            self.after(50, lambda: True if self.server.is_server_pending() else self._connection_close(1))
+            return self.server.is_server_pending()
+        else:
+            self._connection_close()
+            return self.server.is_server_pending()
+
+    def _connection_close(self, case=0):
+        if case == 1:
+            self.status_bar.set_text("Could not establish the connection...", 2000)
+        self.server.stop_recv()
+        self.serial_widget.serial_connect_button_label(0)
+        self.record_play_widget.set_button_record_enable(False)
+        self.record_play_widget.set_button_play_enable(False)
+
+    def _load_data(self):
+        self.load_data.clear()
+        load = tkd.askopenfile()
+        if not load:
+            return False
+
+        for line in load:
+            self.load_data.append(line.rstrip())
+        load.close()
+
+    def _record_data(self):
+        if self.server.is_server_pending():
+            if self.server.is_recording_data():
+                self.server.record_data_stop()
+                return False
+            else:
+                self.server.record_data_start()
+                return True
+        return False
+
+    def _play_data_send_method(self):
+
+        if self.server.is_server_pending() and not self.server.is_recording_data():
+            print("In sending func")
+            if self.load_data and self.isPlaying:
+                send_data = self.load_data.pop(1)
+                self.server.send_data(send_data)
+                self.after(self.sendTimeInterval, self._play_data_send_method)
+                return True
+        print("Nothing to play")
+        self.record_play_widget.button_play_label(0)
+        self.isPlaying = False
+        return False
+
+    def _play_data(self):
+        if self.isPlaying:
+            self.isPlaying = False
+            return False
+        else:
+            self.isPlaying = True
+            return self._play_data_send_method()
 
     def menu_open(self):
         self.exec_command(key="menu_open")
 
     def menu_save(self):
         self.exec_command(key="menu_save")
+        data_list = ss.to_list(self.recv_data)
+        file = tkd.asksaveasfile(mode="w")
+        if not file:
+            return False
+        file.writelines(data_list)
+        file.close()
 
     def menu_about(self):
         self.exec_command(key="menu_about")
+
+    def menu_clear_recv_data(self):
+        with mp.Lock():
+            while not self.recv_data.empty():
+                self.recv_data.get()
+
+    def menu_clear_load_data(self):
+        self.load_data.clear()
+
+    def __del__(self):
+        self.server.join()
+        self.server.terminate()
+
--- serial_server.py
+++ serial_server.py
@@ -1,72 +1,119 @@
 from serial.threaded import *
 from serial.tools import list_ports
 import multiprocessing as mp
+import copy
 import time
+
 
 def to_list(queue: mp.Queue):
     queue_list = [queue.get() for _ in range(queue.qsize()) if queue]
     return queue_list
 
 
-class _SerialReadLine(LineReader):
-    def connection_made(self, transport):
-        super(_SerialReadLine, self).connection_made(transport=transport)
-        print("Connection opened")
-        self.ENCODING = "ascii"
-        self.TERMINATOR = b'\n'
-
-    def add_queue(self, recv_queue: mp.Queue=None):
-        self.recv_queue = recv_queue
-
-    def handle_line(self, line):
-        # print(line)
-        self.recv_queue.put(line)
-
-    def connection_lost(self, exc):
-        if exc:
-            print(exc)
-        print("Connection closed")
-
-
 class SerialServer(mp.Process):
     def __init__(self, port=None, baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
                  stopbits=serial.STOPBITS_ONE, queue: mp.Queue=None):
         mp.Process.__init__(self)
-        self.daemon = True
         self.recv_queue = queue
         self.send_queue = mp.Queue()
-        self.port = port
+        self.port = mp.Queue()
+        self.port.put(port)
         self.baudrate = baudrate
         self.bytesize = bytesize
         self.parity = parity
         self.stopbits = stopbits
         self._lock = mp.Lock()
+        self._running_event = mp.Event()
+        self._running_event.clear()
+        self._alive = mp.Event()
+        self._alive.set()
+        self._recording_data = mp.Event()
+        self._recording_data.clear()
+
         try:
-            self.serv = serial.Serial(port=self.port, baudrate=self.baudrate, bytesize=self.bytesize,
+            self.serv = serial.Serial(port=self.port.get(), baudrate=self.baudrate, bytesize=self.bytesize,
                                       parity=self.parity, stopbits=self.stopbits)
-        except serial.SerialException as e:
-            print(e)
+        except serial.SerialException as ee:
+            print(ee)
 
     def run(self):
-        try:
-            while self.is_alive() and self.serv.is_open:
-                try:
-                    self.recv_queue.put(self.serv.readline().decode("ascii"))
-                except KeyboardInterrupt: break
-                except serial.SerialException as e:
-                    print("Serial {}".format(e))
+        while self._alive.is_set():
+            if not self._running_event.is_set():
+                try: time.sleep(0.05)
+                except:
+                    self._alive.clear()
+                    self._running_event.clear()
+                    self._recording_data.clear()
                     break
-        except AttributeError:
-            raise RuntimeError("No resource")
+            else:
+                self._open_serial()
+                while self.serv.is_open and self._running_event.is_set() and self._recording_data.is_set():
+                    try:
+                        self.recv_queue.put(self.serv.readline().decode("ascii"))
+                    except:
+                        self._running_event.clear()
+                        self._recording_data.clear()
+                        break
+                else: time.sleep(0.05)
+
+    def _open_serial(self):
+        with self._lock:
+            if not self.serv.is_open:
+                s = self.port.get()
+                self.serv.port = s
+                self.serv.baudrate = self.baudrate
+                self.serv.bytesize = self.bytesize
+                self.serv.parity = self.parity
+                self.serv.stopbits = self.stopbits
+                try:
+                    self.serv.open()
+                except Exception as e:
+                    print("Connection error {}".format(e))
+                    self._running_event.clear()
+                    self._recording_data.clear()
+
+    def send_data(self, line):
+        self._open_serial()
+        with self._lock:
+            if self.serv.is_open:
+                try:
+                    self.serv.write("{}\n".format(line).encode("ascii"))
+                except:
+                    self._recording_data.clear()
+
+    def is_server_pending(self):
+        return self._running_event.is_set()
+
+    def start_recv(self):
+        self._running_event.set()
+
+    def stop_recv(self):
+        self._running_event.clear()
+
+    def record_data_start(self):
+        self._recording_data.set()
+
+    def record_data_stop(self):
+        self._recording_data.clear()
+
+    def is_recording_data(self):
+        return self._recording_data.is_set()
+
+    def set_port(self, port: str = None):
+        with self._lock:
+            while not self.port.empty():
+                self.port.get()
+            self.port.put(port)
+
+    def join(self, timeout=None):
+        self._running_event.clear()
+        self._recording_data.clear()
+        self._alive.clear()
+        mp.Process.join(self, timeout)
 
     @staticmethod
     def list_ports():
         return list_ports.comports()
-
-    def send_data(self, line):
-        with self._lock:
-            if self.serv:
-                self.serv.write("{}\n".format(line).encode("ascii"))
 
 
 if __name__ == "__main__":
@@ -76,6 +123,8 @@ if __name__ == "__main__":
     i = 0
     # l = [x for x in range(100001)]
     server.start()
+    server.start_recv()
+    server.record_data_start()
     while True:
         try:
             elem = q.get().rstrip() + " " + str(i)
@@ -90,5 +139,5 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             print("Interrupt")
             break
-
+    server.stop_recv()
     server.join()
